3ef16481e9aba981818469db19433f119205456d
FrancoAdN <francoadinapoli@gmail.com>
2019-09-03 20:30:20 -0300

    add

--- scripts/post.py
+++ scripts/post.py
@@ -1,17 +1,29 @@
-# https://twitter.com/sessions
+from requests import Request, Session
+from bs4 import BeautifulSoup
+# # nickname: "S.cano"
+# # password: "42904558"
+# # token: "36557d4e-a8a3-40b9-a42b-432c567309ae"
 
-# session[username_or_email]: nehudinapoli
-# session[password]: nehu753159852456
-import requests
-# url = 'https://twitter.com/sessions'
-# r = requests.post(url, allow_redirects=True, data={
-#     'session%5Busername_or_email%5D': 'nehudinapoli',
-#     'session%5Bpassword%5D': 'nehu75852456'
-# })
+login_data = {
+    'nickname': 'S.cano',
+    'password': '42904558'
+}
+# # headers = {
+# #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+# # }
+# # with requests.Session() as s: 
+# #     url = 'https://elizalde.alexia.com.ar/ACWeb/WS_Data/WSLogOn.asmx/Validar'
+# #     r = s.get(url, headers=headers)
 
-# print r.url
-url = 'https://elizalde.alexia.com.ar/ACWeb/WS_Data/WSLogOn.asmx/Validar'
-send = {"token":"fe51e861-1d39-4207-af20-bceda1620e29","nickname":"S.cano","password":"42904558"}
+# #     r = s.post(url, data=login_data, allow_redirects=True, headers=headers)
+# #     print r.text
+url = 'https://elizalde.alexia.com.ar/ACWeb/LogOn.aspx'
+s = Session()
+r = s.get(url)
 
-r = requests.post(url, allow_redirects=True, data = send)
-print r
+soup = BeautifulSoup(r.content,features="html.parser")
+login_data['token']= soup.find('input', attrs={'name':'Token'})['value']
+print login_data
+req = Request('POST', url, data=login_data)
+prepped = req.prepare()
+print req.status_code
